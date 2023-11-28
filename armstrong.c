277eaca1e6dee6a22be768a5229c9086e45d3083
Sahillatotra <146922502+Sahillatotra@users.noreply.github.com>
2023-11-28 17:40:59 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool isArmstrong(int num) {
+    int originalNum = num;
+    int sum = 0;
+    int digitCount = 0;
+
+    // Count the number of digits in the input number
+    while (num != 0) {
+        num /= 10;
+        digitCount++;
+    }
+
+    // Reset num to the original value for further processing
+    num = originalNum;
+
+    // Calculate the sum of cubes of digits using mathematical formula
+    while (num != 0) {
+        int digit = num % 10;
+        sum += pow(digit, digitCount);
+        num /= 10;
+    }
+
+    // Check if the sum is equal to the original number
+    return (sum == originalNum);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num)) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}
